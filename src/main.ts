dde8728bb6797cb79f4563cf3d90ed9598de3b27
DNadas98 <daniel.nadas.123@gmail.com>
2024-01-29 02:53:02 +0100

    add jwt, cookie parser and dto validator to the app

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import * as process from "process";
 import * as dotenv from "dotenv";
+import * as cookieParser from "cookie-parser";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,11 @@ async function bootstrap() {
   if (process.env?.NODE_ENV !== "production") {
     dotenv.config();
   }
+
+  app.use(cookieParser());
+
+  // Use the DTO validation for all endpoints
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORTFOLIO_PORT);
 }
