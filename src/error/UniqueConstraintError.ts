0fa5907594681778167cf2d252029a281fb7df79
DNadas98 <daniel.nadas.123@gmail.com>
2024-01-29 02:50:10 +0100

    refactor errors

--- src/error/UniqueConstraintError.ts
+++ src/error/UniqueConstraintError.ts
@@ -1,8 +1,8 @@
-import {CustomError} from "./CustomError";
+import {ConflictException} from "@nestjs/common";
 
-export class UniqueConstraintError extends CustomError {
+export class UniqueConstraintError extends ConflictException {
 
   constructor(message: string) {
-    super(message, 409);
+    super(message);
   }
 }
