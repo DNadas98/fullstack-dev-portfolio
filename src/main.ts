98ad9409d5b290fc216a50be88976def8d44de9c
DNadas98 <daniel.nadas.123@gmail.com>
2024-01-31 16:53:00 +0100

    update ts config to use strict null checking

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./AppModule";
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./AppModule";
 import * as process from "process";
 import * as cookieParser from "cookie-parser";
-import { ValidationPipe } from "@nestjs/common";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORTFOLIO_PORT;
+  if (!port) {
+    throw new Error("Missing port ENV value");
+  }
 
   app.use(cookieParser());
 
   // Use the DTO validation for all endpoints
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORTFOLIO_PORT);
+  await app.listen(port);
 }
 
 bootstrap();
