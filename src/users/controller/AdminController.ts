7532f43b11e6d54a4cc9f8eda6f955bde274a904
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-02 22:37:15 +0100

    extend admin controller

--- src/users/controller/AdminController.ts
+++ src/users/controller/AdminController.ts
@@ -1,30 +1,66 @@
 import {
   BadRequestException,
-  Controller,
+  Controller, Delete,
   Get,
   HttpCode,
   HttpStatus,
-  Param,
+  Param, Patch, Query,
   UseGuards
 } from "@nestjs/common";
-import { UserService } from "../service/UserService";
-import { AuthGuard } from "../../auth/guard/AuthGuard";
-import { RoleGuard, Roles } from "../../auth/guard/RoleGuard";
-import { DataResponseDto } from "../../common/dto/DataResponseDto";
+import {UserService} from "../service/UserService";
+import {AuthGuard} from "../../auth/guard/AuthGuard";
+import {RoleGuard, Roles} from "../../auth/guard/RoleGuard";
+import {DataResponseDto} from "../../common/dto/DataResponseDto";
+import {MessageResponseDto} from "../../common/dto/MessageResponseDto";
 
 @UseGuards(AuthGuard, RoleGuard)
 @Controller("api/v1/admin")
 export class AdminController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+  }
+
+  @Get("users")
+  @HttpCode(HttpStatus.OK)
+  @Roles("ADMIN")
+  async getAllUsers() {
+    const users = await this.userService.readAll();
+    return new DataResponseDto(users);
+  }
 
   @Get("users/:id")
-  @Roles("ADMIN")
   @HttpCode(HttpStatus.OK)
+  @Roles("ADMIN")
   async getUserDetailsById(@Param("id") id: string) {
     if (!id || isNaN(parseInt(id))) {
       throw new BadRequestException();
     }
     const userDetails = await this.userService.readById(+id);
     return new DataResponseDto(userDetails);
+  }
+
+  @Patch("users/:id")
+  @HttpCode(HttpStatus.OK)
+  @Roles("ADMIN")
+  async updateIsActiveById(@Param("id") id: string, @Query("setActive") setActive: string) {
+    if (!id || isNaN(parseInt(id))) {
+      throw new BadRequestException();
+    }
+    if (!setActive || (setActive !== "true" && setActive !== "false")) {
+      throw new BadRequestException();
+    }
+    const active = setActive === "true";
+    const updated = await this.userService.updateIsActive(+id, active);
+    return new DataResponseDto(updated);
+  }
+
+  @Delete("users/:id")
+  @HttpCode(HttpStatus.OK)
+  @Roles("ADMIN")
+  async deleteUserById(@Param("id") id: string) {
+    if (!id || isNaN(parseInt(id))) {
+      throw new BadRequestException();
+    }
+    await this.userService.deleteById(+id);
+    return new MessageResponseDto(`User account with ID ${id} has been deleted successfully`);
   }
 }
