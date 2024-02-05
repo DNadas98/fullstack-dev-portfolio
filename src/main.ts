a45b51820b8e1747cc0ab00b73dcf62dcc632c2e
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-05 09:16:22 +0100

    add security headers with helmet js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./AppModule";
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./AppModule";
 import * as process from "process";
 import * as cookieParser from "cookie-parser";
-import { ValidationPipe } from "@nestjs/common";
+import {ValidationPipe} from "@nestjs/common";
+import helmet from "helmet";
+import {helmetConfig} from "./common/config/helmetConfig";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
     throw new Error("Missing port ENV value");
   }
 
+  // Set security headers
+  app.use(helmet(helmetConfig));
+
+  // Cookie parser
   app.use(cookieParser());
 
   // Use the DTO validation for all endpoints
