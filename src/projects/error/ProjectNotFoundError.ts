53dfb27e6ed02549c6aaa53bd4861beefaf8084a
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-06 14:18:22 +0100

    add exception classes

---
+++ src/projects/error/ProjectNotFoundError.ts
@@ -0,0 +1,7 @@
+import {NotFoundException} from "@nestjs/common";
+
+export class ProjectNotFoundError extends NotFoundException {
+  constructor(message: string = "Project with the provided details was not found") {
+    super(message);
+  }
+}
