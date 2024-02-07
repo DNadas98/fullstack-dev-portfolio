2d9facc3cdd4b146436294853ac5a78abf663ec8
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-07 19:10:13 +0100

    add global rate limiter for interacting with the GitHub API

--- src/common/config/rateLimiterOptions.ts
+++ src/common/config/rateLimiterOptions.ts
@@ -32,3 +32,17 @@ export const mailRateLimiterOptions: ThrottlerOptions = {
   ttl: 60000,
   limit: 3
 };
+
+/**
+ * Used at endpoints interacting with the GitHub REST API<br>
+ * ttl: 3600000 [ms] = 1 h - time to live, the time window<br>
+ * limit: 4990: amount of requests allowed in the TTL<br>
+ * The official docs specify 5000 requests / hour as rate limit with authentication<br>
+ * This rate limit is applied globally, not per user
+ * @link https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28#primary-rate-limit-for-authenticated-users
+ */
+export const githubApiRateLimiterOptions: ThrottlerOptions = {
+  ttl: 3600000,
+  limit: 4990,
+  generateKey: () => "github_global_limit"
+};
