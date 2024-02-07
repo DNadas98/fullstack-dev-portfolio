350a9da620335f4e9fce049e65532efa6cdcf1fa
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-07 13:34:08 +0100

    apply DTO Converter to AuthService

--- src/auth/AuthModule.ts
+++ src/auth/AuthModule.ts
@@ -6,9 +6,13 @@ import {AuthController} from "./controller/AuthController";
 import {CustomJwtModule} from "./CustomJwtModule";
 import {ConfigModule} from "@nestjs/config";
 import {PasswordEncoderModule} from "./PasswordEncoderModule";
+import {ConverterModule} from "../common/converter/ConverterModule";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UsersModule, CustomJwtModule, PasswordEncoderModule],
+  imports: [
+    ConfigModule, DatabaseModule, ConverterModule, UsersModule,
+    CustomJwtModule, PasswordEncoderModule
+  ],
   providers: [
     AuthService
   ],
