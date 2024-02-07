34e8aba9448adafc95980bcfb3ac33a2b6da5cfa
DNadas98 <daniel.nadas.123@gmail.com>
2024-02-07 19:09:41 +0100

    refactor request forwarder

--- src/forward/service/ForwardService.ts
+++ src/forward/service/ForwardService.ts
@@ -11,28 +11,33 @@ export class ForwardService {
   /**
    * Forwards requests to the GitHub REST API returns the response
    * @param req The original request
+   * @param path The part of the URL after the GitHub REST API base URL
    * @link https://docs.github.com/en/rest?apiVersion=2022-11-28
    * @see axios.request
    * @see AxiosRequestConfig
    * @throws AxiosError
    */
-  async forwardGitHubApiRequest(req: Request): Promise<any> {
-    const path = req.url.split("/github/")[1];
+  async forwardGitHubApiRequest(req: Request, path: string): Promise<any> {
     const baseUrl = this.configService.get("PORTFOLIO_GITHUB_API_BASE_URL");
     const token = this.configService.get("PORTFOLIO_GITHUB_API_TOKEN");
 
-    const config: AxiosRequestConfig = {
+    const config = this.getRequestConfig(req, baseUrl, path, token);
+
+    const apiResponse = await axios.request(config);
+    return apiResponse.data;
+  }
+
+  private getRequestConfig(
+    req: Request, baseUrl: string, path: string, token: string
+  ): AxiosRequestConfig {
+    return {
       method: req.method,
       url: `${baseUrl}/${path}`,
       headers: {
         "Content-Type": "application/json",
         ...(token ? {Authorization: `Bearer ${token}`} : {})
       },
-      timeout: 20000,
-      timeoutErrorMessage: "Request to GitHub REST API timed out"
+      timeout: 20000, timeoutErrorMessage: "Request to GitHub REST API timed out"
     };
-
-    const apiResponse = await axios.request(config);
-    return apiResponse.data;
   }
 }
